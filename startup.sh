35e9741b709bc6196b6fff7018bad91becff9e77
“cocoaroger” <cocoaroger@163.com>
2026-06-29 17:56:11 +0800

    调整启动日志

--- startup.sh
+++ startup.sh
@@ -10,3 +10,4 @@ sleep 2 # 等待2秒确保完全释放端口和内存
 echo "正在启动新的交易进程..."
 nohup python examples/live_trading.py --log-level DEBUG > output.log 2>&1 &
 echo "启动成功！当前最新的进程信息如下："
+ps -ef | grep "examples/live_trading.py" | grep -v grep
